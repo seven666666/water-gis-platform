7a336d2ab3b71cbfc07fa271d619c2e341d05e08
root <root@localhost.localdomain>
2026-03-06 14:03:30 +0800

    fix: 使用vite-plugin-cesium解决兼容问题

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cesium from 'vite-plugin-cesium'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    cesium()
+  ],
   server: {
     host: '0.0.0.0',
     port: 5174
-  },
-  cesium: {
-    cesiumBaseUrl: 'https://cesium.com/downloads/cesiumjs/releases/1.104/Build/Cesium/'
   }
 })
